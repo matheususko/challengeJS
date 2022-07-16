218265822bd13cc7e1d4a649aea158c470ff424e
Matheus Susko <matheususko@gmail.com>
2022-07-16 11:52:09 -0300

    First commit day 01

---
+++ day01/constante-var/app.js
@@ -0,0 +1,19 @@
+// let permite reatribuição de valores
+let age = 22
+let currentYear = 2022
+
+console.log(age, currentYear)
+
+age = 23
+console.log(age)
+
+// const impede uma reatribuição
+const points = 100
+// points = 101 
+console.log(points)
+
+// var aceita reatribuição
+var score = 74
+console.log(score)
+score = 75
+console.log(score)
